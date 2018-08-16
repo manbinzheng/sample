b69a196a6148427fa132d44ddef40cbc5cca7a2a
vagrant <vagrant@homestead>
2018-08-16 03:22:05 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,6 +9,7 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
@@ -26,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
-  </head>
-  <body>
-    @include('layouts._header')
-    <div class="container">
-       <div class="col-md-offset-1 col-md-10">
+</head>
+<body>
+@include('layouts._header')
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
-      </div>
-    </div>    
-  </body>
+    </div>
+</div>
+</body>
 </html>
