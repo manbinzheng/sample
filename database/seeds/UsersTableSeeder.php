563898350505001bde2a427b560dbcf7db8696ee
vagrant <vagrant@homestead>
2018-08-20 08:57:08 +0000

    Add user following

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,4 +23,18 @@ class UsersTableSeeder extends Seeder
         $user->activated = true;
         $user->save();
     }
+
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
 }
